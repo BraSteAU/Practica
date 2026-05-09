937bd90d4905114e491d707b951a4c12b82aba5d
BraSteAU <brayan_8924@hotmail.com>
2026-05-08 21:30:26 -0500

    Agrgue las clase Padre Vehiculo y las clases hijas

---
+++ camionetaSUV.java
@@ -0,0 +1,29 @@
+package Practica;
+
+public class camionetaSUV extends Vehiculo {
+    private String tipoTraccion; //4x2 4x4
+    private int capacidadPasajeros; //litros
+
+    public camionetaSUV() {
+        super();
+    }
+
+    public camionetaSUV(String tipoTraccion, int capacidadPasajeros) {
+        this.tipoTraccion = tipoTraccion;
+        this.capacidadPasajeros = capacidadPasajeros;
+    }
+
+    public String getTipoTraccion() {
+        return tipoTraccion;
+    }
+    public void setTipoTraccion(String tipoTraccion) {
+        this.tipoTraccion = tipoTraccion;
+    }
+    public int getCapacidadPasajeros() {
+        return capacidadPasajeros;
+    }
+    public void setCapacidadPasajeros(int capacidadPasajeros) {
+        this.capacidadPasajeros = capacidadPasajeros;
+    }
+    
+}
