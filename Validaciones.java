4f03f0de47942efccd29dd271efbfeda5a090322
BraSteAU <brayan_8924@hotmail.com>
2026-05-29 22:01:51 -0500

    Actualizacion

--- Validaciones.java
+++ Validaciones.java
@@ -18,4 +18,50 @@ public class Validaciones {
         System.out.println("Datos invalidos, solo se permiten letras");
         return validarTexto(sc);
     }
+
+    public String validarDireccion(Scanner sc){
+        String direccion = sc.nextLine();
+        if(!direccion.trim().isEmpty()){
+            return direccion;
+        }
+        System.out.println("La direccion no puede estar vacia");
+        return validarDireccion(sc);
+    }
+
+    public String validarNumerosAlfabeto(Scanner sc, String campo){
+        String dato = sc.next();
+        if(dato.matches("[a-zA-Z0-9]+")){
+            return dato;
+        }
+        System.out.println(campo+" Invalido, no se permiten caracteres especiales");
+        return validarNumerosAlfabeto(sc, campo);
+    }
+
+    public int validarEnteroPositivo(Scanner sc){
+        if(!sc.hasNextInt()){
+            System.out.println("Ingrese un numero entero valido");
+            sc.next();
+            return validarEnteroPositivo(sc);
+        }   
+        int n = sc.nextInt();
+        if(n>0){
+            return n;
+        }
+        System.out.println("El numero debe ser positivo");
+        return validarEnteroPositivo(sc);
+    }
+
+    public float validarFloatPositivo(Scanner sc, String campo) {
+        if (!sc.hasNextFloat()) {
+            System.out.println(campo + " Invalido. Ingrese un numero.");
+            sc.next();
+            return validarFloatPositivo(sc, campo);
+        }
+        float numero = sc.nextFloat();
+        if (numero > 0) {
+            return numero;
+        }
+        System.out.println(campo + " Invalido. Debe ser positivo.");
+        return validarFloatPositivo(sc, campo);
+    }
 }
